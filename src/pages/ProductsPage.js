9fbd87674f992319c4d8ad871cddf38d9b19a09d
Ariel Cardoso <arielvix@gmail.com>
2020-11-13 16:37:12 -0300

    Add products

--- src/pages/ProductsPage.js
+++ src/pages/ProductsPage.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
+import listItem from '../utils/ProductList'
 
 const Products = () => {
   return (
@@ -6,6 +8,18 @@ const Products = () => {
       <div className="page-header">
         <div className='container'>
           <h1>Products Page</h1>
+        </div>
+      </div>
+
+      <div className='container'>
+        <div className='product-list'>
+          {listItem.map((item, index) => (
+            <Link to={`product/${item.sku}`} id={index} className='product-item' >
+              <img src={item.image} alt={item.name}/>
+              <div className="title">{item.name}</div>
+              <div className="price"><small>CAD</small> {item.price}</div>
+            </Link>
+          ))}
         </div>
       </div>
     </>
---
+++ src/utils/ProductList.js
@@ -0,0 +1,68 @@
+import p01 from '../assets/images/products/01.jpg';
+import p02 from '../assets/images/products/02.jpg';
+import p03 from '../assets/images/products/03.jpg';
+import p04 from '../assets/images/products/04.jpg';
+import p05 from '../assets/images/products/05.jpg';
+import p06 from '../assets/images/products/06.jpg';
+import p07 from '../assets/images/products/07.jpg';
+import p08 from '../assets/images/products/08.jpg';
+import p09 from '../assets/images/products/09.jpg';
+
+const ProductList = [
+    {
+        name: "Premium Strawberry Pie", 
+        sku: 101, 
+        price: 29.00, 
+        image: p01
+    },
+    {
+        name: "Premium Apple Pie", 
+        sku: 102, 
+        price: 29.00, 
+        image: p02
+    },
+    {
+        name: "Premium Orange Pie", 
+        sku: 103, 
+        price: 29.00, 
+        image: p03
+    },
+    {
+        name: "Banana Pie", 
+        sku: 104, 
+        price: 15.00, 
+        image: p04
+    },
+    {
+        name: "Nut Pie", 
+        sku: 105, 
+        price: 25.00, 
+        image: p05
+    },
+    {
+        name: "Special Raspberry Pie", 
+        sku: 106, 
+        price: 25.00, 
+        image: p06
+    },
+    {
+        name: "Special Salt Pie", 
+        sku: 107, 
+        price: 25.00, 
+        image: p07
+    },
+    {
+        name: "Avocado Pie", 
+        sku: 108, 
+        price: 15.00, 
+        image: p08
+    },
+    {
+        name: "Green Salt Pie", 
+        sku: 109, 
+        price: 15.00, 
+        image: p09
+    }
+];
+
+export default ProductList;
